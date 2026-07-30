13723637e11114ee982fc0357b7fc415bfbda792
subhan513 <subhancs57@gmail.com>
2026-07-30 23:49:51 +0500

    done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,7 @@ import ListingItem from "../components/ListingItem"
 const Home = () => {
 
 
-const VITE_API_URL = import.meta.env.VITE_API_URL;
+const VITE_API_URL = import.meta.env.VITE_API_URL || "/api";
 console.log("API URL:", import.meta.env.VITE_API_URL);
   SwiperCore.use([Navigation]) // FIX: Use array
   const [offerlistings, setofferlistings] = useState([]);
